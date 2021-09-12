fa1d207a820946a0761fe896487a54633dfec5b7
atosystem <atosystem@hotmail.com>
2021-09-12 15:43:15 +0800

    Updates

--- component---src-pages-about-js-d7b9ea16f35b06571182.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[8682],{5518:function(e,t,a){"use strict";a.r(t);var u=a(7294),n=a(5444),r=a(7849),l=a(6179);a(1496);t.default=function(){(0,n.K2)("1777579754");return u.createElement(r.Z,null,u.createElement(l.Z,{title:"About"}),u.createElement("h1",{onLoad:void 0},"About"))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-d7b9ea16f35b06571182.js.map
